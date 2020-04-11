4e91a5590242bce6b01b62b6b918d8bb27645873
Kwang-Su Kim <atelier.jullian@gmail.com>
2020-04-11 01:11:28 +0100

    update style

--- plugins/ga.js
+++ plugins/ga.js
@@ -30,7 +30,7 @@ export default ({ app }) => {
   /*
    ** Set the current page
    */
-  ga('create', 'UA-143778881-2', 'auto')
+  ga('create', 'UA-XXXXXXXX-X', 'auto')
   /*
    ** Every time the route changes (fired on initialization too)
    */
